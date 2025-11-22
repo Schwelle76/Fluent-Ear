b456665455219de3dc20a4547faaa1f6f9dc560b
Mattes <mattes.zens@web.de>
2025-11-22 19:02:15 +0100

    FIX: Audioplayer not scratchy on mobile anymore

--- src/services/SoundFontService.ts
+++ src/services/SoundFontService.ts
@@ -4,26 +4,38 @@ import Soundfont, { InstrumentName, Player } from "soundfont-player";
 export default class SoundfontService {
   private ctx: AudioContext;
   private player: Player | null = null;
+  private masterGain: GainNode;
+
   constructor(ctx?: AudioContext) {
     this.ctx = ctx ?? new (window.AudioContext || (window as any).webkitAudioContext)();
+
+    this.masterGain = this.ctx.createGain();
+    this.masterGain.gain.value = 25;
+    this.masterGain.connect(this.ctx.destination);
 
   }
 
   async load(instrument: InstrumentName = "acoustic_grand_piano") {
     this.player = await Soundfont.instrument(this.ctx, instrument);
+
+    if (typeof this.player.connect === "function") {
+      this.player.connect(this.masterGain);
+    }
+
     return this.player != null;
   }
 
-  async play (notes: string | string[], duration: number = 0.5, release: number = 0.1): Promise<boolean> {
+  async play(notes: string | string[], duration: number = 0.5, release: number = 0.1): Promise<boolean> {
+
     await this.resumeIfNeeded();
-    
+
     if (!this.player) return false;
 
     if (typeof notes === "string")
       notes = [notes];
 
-    for(const note of notes) {
-      this.player.play(note, 0, { duration, release, gain: 40});
+    for (const note of notes) {
+      this.player.play(note, 0, { duration, release, gain: 1 });
     }
 
     return new Promise<boolean>((resolve) => {
@@ -39,6 +51,9 @@ export default class SoundfontService {
   }
 
   destroy() {
+
+    this.masterGain.disconnect();
+
     if (this.ctx && this.ctx.state !== "closed") {
       this.ctx.close().catch(() => { });
     }
